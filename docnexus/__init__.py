bb98c7722cb847f8b33f8e1bea15bc69837e9abe
Suresh Kumar <mr.sutharsuresh@gmail.com>
2025-12-28 20:52:54 +0530

    Release: DocNexus v1.1.0 - Configurable Workspace & UI Polish

--- docnexus/__init__.py
+++ docnexus/__init__.py
@@ -2,7 +2,7 @@
 DocNexus - The Ultimate All-in-One Document Engine
 """
 
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 __author__ = 'OmniDoc Maintainers'
 __email__ = 'maintainers@omnidoc.org'
 __description__ = 'OmniDoc - The Ultimate All-in-One Document Engine'
